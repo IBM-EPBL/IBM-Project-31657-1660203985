6aa16abcc675c8c34bea62717b4ada17991500c9
Vishwa-30 <117158268+Vishwa-30@users.noreply.github.com>
2022-11-06 00:04:00 +0530

    Add files via upload

---
+++ Vishwa/Assignment 2/flask.py.py
@@ -0,0 +1,12 @@
+from flask import Flask;
+from flask import render_template,url,redirect;
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return render_template('login.html')
+
+@app.route('/signup')
+def show():
+    return render_template('registerpg.html')
